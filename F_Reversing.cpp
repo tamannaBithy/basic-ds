6e3c6c37cb65c46e445cff4b8be72dbaf8e31ecc
Tamanna14-cpu <bimurto.bithy14@gmail.com>
2024-12-10 17:32:10 +0600

    F_Reversing

---
+++ F_Reversing.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+
+    reverse(a.begin(), a.end());
+    for (int x : a)
+    {
+        cout << x << " ";
+    }
+
+    return 0;
+}
