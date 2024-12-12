87670bbd1e0138899fd36c362a43d9d59d27eb3a
Tamanna14-cpu <bimurto.bithy14@gmail.com>
2024-12-12 23:48:07 +0600

    Z_Binary Search

--- Z_Binary_Search.cpp
+++ Z_Binary_Search.cpp
@@ -3,6 +3,49 @@ using namespace std;
 
 int main()
 {
+    int n, q;
+    cin >> n >> q;
+    int a[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
 
+    sort(a, a + n);
+
+    for (int i = 0; i < q; i++)
+    {
+        int val;
+        cin >> val;
+        int flag = 0;
+        int l = 0;
+        int r = n - 1;
+        while (r >= l)
+        {
+            int mid = (l + r) / 2;
+            if (a[mid] == val)
+            {
+                flag = 1;
+                break;
+            }
+            else if (a[mid] > val)
+            {
+                r = mid - 1;
+            }
+            else
+            {
+                l = mid + 1;
+            }
+        }
+
+        if (flag)
+        {
+            cout << "found" << endl;
+        }
+        else
+        {
+            cout << "not found" << endl;
+        }
+    }
     return 0;
 }
