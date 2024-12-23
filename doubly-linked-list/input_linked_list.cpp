41c72e9b20c4499780c284b7d5a91e9e02996ff7
Tamanna14-cpu <bimurto.bithy14@gmail.com>
2024-12-23 20:32:42 +0800

    input_linked_list

---
+++ doubly-linked-list/input_linked_list.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int val;
+    Node *next;
+    Node *prev;
+    Node(int val)
+    {
+        this->val = val;
+        this->next = NULL;
+        this->prev = NULL;
+    }
+};
+
+void insert_tail(Node *&head, Node *&tail, int val)
+{
+    Node *newNode = new Node(val);
+    if (tail == NULL)
+    {
+        head = newNode;
+        tail = newNode;
+        return;
+    }
+    newNode->prev = tail;
+    tail->next = newNode;
+    tail = newNode;
+}
+
+void print_linked_list(Node *head)
+{
+    Node *tmp = head;
+    while (tmp != NULL)
+    {
+        cout << tmp->val << " ";
+        tmp = tmp->next;
+    }
+    cout << endl;
+}
+
+int main()
+{
+    Node *head = NULL;
+    Node *tail = NULL;
+    int val;
+    while (true)
+    {
+        cin >> val;
+        if (val == -1)
+            break;
+
+        insert_tail(head, tail, val);
+    }
+
+    print_linked_list(head);
+
+    return 0;
+}
