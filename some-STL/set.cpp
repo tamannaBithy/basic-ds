2d1e9b358c9c6f5095b21eca903d46f196781b29
Tamanna14-cpu <bimurto.bithy14@gmail.com>
2025-01-25 23:45:54 +0800

    set

---
+++ some-STL/set.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    set<int> s;
+    int n;
+    cin >> n;
+    while (n--)
+    {
+        int val;
+        cin >> val;
+        s.insert(val); // o(logN)
+    }
+
+    for (auto it = s.begin(); it != s.end(); it++)
+    {
+        cout << *it << " "; /// o(logN)
+    }
+    cout << endl;
+
+    if (s.count(4))
+        cout << "ache" << endl;
+    else
+        cout << "nai" << endl;
+    return 0;
+}
